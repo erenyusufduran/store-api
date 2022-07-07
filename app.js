f004ead0a28f7f73aa88f4fa5a5d717cc4e75547
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-07 14:10:21 +0300

    router and controller connection

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const connectDB = require("./db/connect");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const notFoundMiddleware = require("./middleware/not-found");
 
+const productsRouter = require("./routes/products");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products"></a>');
 });
+
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 async function startServer() {
   try {
