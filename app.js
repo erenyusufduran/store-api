7d624decb0deaad7efece334ef483d21cb4aa31a
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-07 14:03:21 +0300

    start server and mongo connections

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./db/connect");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products"></a>');
+});
+
+async function startServer() {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+startServer();
